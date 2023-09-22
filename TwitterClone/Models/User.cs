4bb87fb46b562c0426cf3694c5fb447d10c25a50
repoleved08 <geekbii08@gmail.com>
2023-09-22 19:18:59 +0300

    Beautify UI

--- TwitterClone/Models/User.cs
+++ TwitterClone/Models/User.cs
@@ -4,5 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
     }
 }
